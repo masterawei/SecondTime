f1478f52341b72dd5b64895efa6663935c7647db
masterawei <luckinaline@gmail.com>
2021-12-10 14:21:54 +0800

    leetcode 738

---
+++ src/Greedy/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,22 @@
+package Greedy;
+
+/**
+ * @author away
+ * @date 2021-12-10 11:11
+ */
+public class BestTimeToBuyAndSellStockWithTransactionFee {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int buy = prices[0] + fee;
+        int profit = 0;
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] + fee < buy) {
+                buy = prices[i] + fee;
+            } else if (prices[i] > buy) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+}
